79677ac0af782a49a8b8eb00bce9005ce5986615
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-06 21:57:38 +0200

    initial commit

---
+++ word-counter.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Word Counter
+Plugin URI: https://wordpress.org/plugins/word-counter
+Description: A plugin that counts words
+Version: 0.1.0
+Author: Me
+Author URI: http://example.com
+Text Domain: word-counter
+Domain Path: /languages
+*/
