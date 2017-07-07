5876df91b4c8634220179c1ecbd97b013709e492
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-07 19:34:52 +0200

    step 6

--- tests/functional/AverageWPMCest.php
+++ tests/functional/AverageWPMCest.php
@@ -17,4 +17,25 @@ class AverageWPMCest {
 	public function should_have_an_average_wpm_value_of_n_a_when_there_are_no_posts( FunctionalTester $I ) {
 		$I->assertEquals( 'n/a', $I->grabOptionFromDatabase( 'wcounter_average_wpm' ) );
 	}
+
+	/**
+	 * It should correctly store the average WPM value when creating posts
+	 *
+	 * @test
+	 */
+	public function should_correctly_store_the_average_wpm_value_when_creating_posts( FunctionalTester $I ) {
+		$I->loginAsAdmin();
+
+		$i = $words = 0;
+		foreach ( [ 6, 4, 2 ] as $n ) {
+			$words += $n;
+			$I->amOnAdminPage( '/post-new.php' );
+			$I->submitForm( '#post', [
+				'post_title' => 'Post ' . ++ $i,
+				'content'    => implode( ' ', array_fill( 1, $n, 'lorem' ) ),
+			] );
+
+			$I->assertEquals( "{$words}/{$i}", $I->grabOptionFromDatabase( 'wcounter_average_wpm' ) );
+		}
+	}
 }
--- word-counter.php
+++ word-counter.php
@@ -14,6 +14,34 @@ register_activation_hook( __FILE__, function () {
 	update_option( 'wcounter_average_wpm', 'n/a' );
 } );
 
+add_action( 'wp_insert_post', function ( $post_id, WP_Post $post, $update ) {
+	if ( 'post' !== $post->post_type || ! empty( $post->post_parent ) ) {
+		return;
+	}
+
+	$current_value = get_option( 'wcounter_average_wpm' );
+
+	if ( 'n/a' === $current_value ) {
+		$total_words = $total_posts = 0;
+	} else {
+		$frags       = explode( '/', $current_value );
+		$total_words = count( $frags ) === 2 ? intval( $frags[0] ) : 0;
+		$total_posts = count( $frags ) === 2 ? intval( $frags[1] ) : 0;
+	}
+
+	$content = apply_filters( 'the_content', $post->post_content );
+	$words   = str_word_count( strip_tags( $content, '' ) );
+
+	if ( $words === 0 ) {
+		return;
+	}
+
+	$total_words += $words;
+	$total_posts += 1;
+
+	update_option( 'wcounter_average_wpm', "{$total_words}/{$total_posts}" );
+}, 10, 3 );
+
 add_filter( 'the_title', function ( $title, $post_id ) {
 	$post = get_post( $post_id );
 
